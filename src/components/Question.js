174b32390dc4235af40c6c2ab2ac08c15d076061
Jerrica Bobadilla <jbobadilla27@gmail.com>
2022-09-19 08:39:19 -0700

    initialize files

---
+++ src/components/Question.js
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+
+function Question({ question, onAnswered }) {
+  const [timeRemaining, setTimeRemaining] = useState(10);
+
+  // add useEffect code
+
+  function handleAnswer(isCorrect) {
+    setTimeRemaining(10);
+    onAnswered(isCorrect);
+  }
+
+  const { id, prompt, answers, correctIndex } = question;
+
+  return (
+    <>
+      <h1>Question {id}</h1>
+      <h3>{prompt}</h3>
+      {answers.map((answer, index) => {
+        const isCorrect = index === correctIndex;
+        return (
+          <button key={answer} onClick={() => handleAnswer(isCorrect)}>
+            {answer}
+          </button>
+        );
+      })}
+      <h5>{timeRemaining} seconds remaining</h5>
+    </>
+  );
+}
+
+export default Question;
